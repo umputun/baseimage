a5ca57de3284af194591414ddf3a9c1a77d87884
Umputun <umputun@gmail.com>
2018-06-10 19:50:39 -0500

    short rev to 7

--- build.go/git-rev.sh
+++ build.go/git-rev.sh
@@ -8,12 +8,13 @@ head_full=$(cat .git/HEAD)
 head_full_elems=(${head_full//:/ })
 ref=${head_full_elems[1]}
 
+# get last elements from ref value, i.e. "refs/heads/master"
 head_short=(${head_full_elems[1]//// })
 head=${head_short[@]: -1:1}
 
 # get hash
 full_hash=$(cat .git/$ref)
-short_hash=${full_hash:0:8}
+short_hash=${full_hash:0:7}
 
 # get time stamp
 log=$(tail -n1 .git/logs/$ref)
