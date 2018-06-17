50939b1ca22550c850105b5ec0b5e0141a8d8e68
Umputun <umputun@gmail.com>
2018-06-17 00:41:16 -0500

    support name with whitespace #1

--- build.go/git-rev.sh
+++ build.go/git-rev.sh
@@ -18,6 +18,17 @@ short_hash=${full_hash:0:7}
 # get time stamp
 log=$(tail -n1 .git/logs/$ref)
 log_elems=(${log// / })
-ts=$(date -d "@${log_elems[4]}" +'%Y%m%dT%H:%M:%S')
+
+# 0000000000000000 8d39c3e9d85d044c7f5f Eugene <eugene@umputun.com> 1529212777 -0500	commit (initial): rev 1234
+e=${log_elems[3]}
+if [[ ${e:0:1} == "<" ]]; then
+    ts=$(date -d "@${log_elems[4]}" +'%Y%m%dT%H:%M:%S')
+fi
+
+# 0000000000000000 8d39c3e9d85d044c7f5f Eugene Umputun <eugene@umputun.com> 1529212777 -0500	commit (initial): rev 1234
+e=${log_elems[4]}
+if [[ ${e:0:1} == "<" ]]; then
+    ts=$(date -d "@${log_elems[5]}" +'%Y%m%dT%H:%M:%S')
+fi
 
 echo $head-$short_hash-$ts
