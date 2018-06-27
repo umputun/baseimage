e9edefed928927fd1d25a298a1fa6fd24990fbfd
Umputun <umputun@gmail.com>
2018-06-27 00:04:04 -0500

    fix app.sh to be alpine friendly

--- base.alpine/files/scripts/app.sh
+++ base.alpine/files/scripts/app.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $# -eq 0 ]
-  then
+cd /srv
+
+if [[ $# -eq 0 ]]; then
     export TERM=xterm-color
 else
     export TERM=$1
 fi
 
-if [[ $EUID -ne 0 ]]; then
-   exec /bin/bash
+uid=$(id -u)
+if [[ $uid -ne 0 ]]; then
+   exec /bin/sh
 fi
 
 su app
