b4a9c8c8316301af2c43feff4625eda6d71fbf0d
Umputun <umputun@gmail.com>
2021-05-04 15:13:58 -0500

    add versions.sh to simplify inside and outside of CI version detection

---
+++ build.go/version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+if [ -z "$CI" ] ; then
+  #outside of 
+  ref=$(git describe --tags --exact-match 2> /dev/null || git symbolic-ref -q --short HEAD)
+  version="${ref}"-$(git log -1 --format=%h)-$(date +%Y%m%dT%H:%M:%S)
+else
+  ref="$(echo ${GITHUB_REF} | cut -d'/' -f3)"
+  version=${ref}-${GITHUB_SHA:0:7}-$(date +%Y%m%dT%H:%M:%S)
+fi
+
+echo "$version"
