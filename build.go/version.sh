2b535bc7fcab628b17ae8c978be862363564b68d
Umputun <umputun@gmail.com>
2021-05-04 15:14:33 -0500

    fix comments

--- build.go/version.sh
+++ build.go/version.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
 if [ -z "$CI" ] ; then
-  #outside of 
+  # outside of CI
   ref=$(git describe --tags --exact-match 2> /dev/null || git symbolic-ref -q --short HEAD)
   version="${ref}"-$(git log -1 --format=%h)-$(date +%Y%m%dT%H:%M:%S)
 else
+  # outside of CI (gh actions)
   ref="$(echo ${GITHUB_REF} | cut -d'/' -f3)"
   version=${ref}-${GITHUB_SHA:0:7}-$(date +%Y%m%dT%H:%M:%S)
 fi
