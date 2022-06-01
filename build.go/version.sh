40677b819d9b4b2082a5d0e826a46a312a88dfc8
Umputun <umputun@gmail.com>
2022-06-01 01:39:34 -0500

    support git-less version default

--- build.go/version.sh
+++ build.go/version.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env sh
 
+version="local-$(date +%Y%m%dT%H:%M:%S)"
+
 if [ -z "$CI" ] ; then
   # outside of CI
-  ref=$(git describe --tags --exact-match 2> /dev/null || git symbolic-ref -q --short HEAD)
-  version="${ref}"-$(git log -1 --format=%h)-$(date +%Y%m%dT%H:%M:%S)
+  if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = "true" ] ; then
+    # valid git repo
+    ref=$(git describe --tags --exact-match 2> /dev/null || git symbolic-ref -q --short HEAD)
+    version="${ref}"-$(git log -1 --format=%h)-$(date +%Y%m%dT%H:%M:%S)
+  fi
 else
   # inside CI (gh actions)
   ref="$(echo ${GITHUB_REF} | cut -d'/' -f3)"
