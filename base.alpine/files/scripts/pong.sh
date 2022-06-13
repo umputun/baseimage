91cfc4b66ab20e9aaba3fe407f74a21e92eac36a
Umputun <umputun@gmail.com>
2022-06-13 14:54:58 -0500

    add pong server

---
+++ base.alpine/files/scripts/pong.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# this is a stub server, for ping requests. Listening on port 8080.
+info=$1
+
+echo "HTTP/1.1 200 OK" > /tmp/pong.http
+echo "Content-Type: text/html; charset=UTF-8" >> /tmp/pong.http
+echo "Server: ${info}" >> /tmp/pong.http
+echo >> /tmp/pong.http
+echo "pong"  >> /tmp/pong.http
+cat /tmp/pong.http
+
+while true; do cat /tmp/pong.http | nc -l 8080; done
