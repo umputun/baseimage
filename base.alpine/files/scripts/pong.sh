7d1cc8b3475e49148f103355be62bcc8c48418c0
Umputun <umputun@gmail.com>
2022-06-13 15:43:02 -0500

    missing port number for pong stub

--- base.alpine/files/scripts/pong.sh
+++ base.alpine/files/scripts/pong.sh
@@ -3,6 +3,8 @@
 # this is a stub server, for ping requests. Listening on port 8080.
 info=$1
 
+echo "ping-pong server, listening on port 8080 ${info}"
+
 echo "HTTP/1.1 200 OK" > /tmp/pong.http
 echo "Content-Type: text/html; charset=UTF-8" >> /tmp/pong.http
 echo "Server: ${info}" >> /tmp/pong.http
@@ -10,4 +12,4 @@ echo >> /tmp/pong.http
 echo "pong"  >> /tmp/pong.http
 cat /tmp/pong.http
 
-while true; do cat /tmp/pong.http | nc -l 8080; done
+while true; do cat /tmp/pong.http | nc -l -p 8080; done
